5efbc825785d0f3e6ac1c1e435ab8b0e7437ccd2
Daniel Alferes <dancon.alferes@gmail.com>
2024-10-03 18:33:11 -0300

    Implementando as rotas principal, sobre-nos e contato

--- Code/app_super_gestao/routes/web.php
+++ Code/app_super_gestao/routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá, seja bem vindo ao curso!';
+});
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre nos';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
 });
