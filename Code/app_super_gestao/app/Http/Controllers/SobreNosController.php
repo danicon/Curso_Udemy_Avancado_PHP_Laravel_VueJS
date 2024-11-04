77cde8937ba829f0fe968e8a3b9fc7df50175970
Daniel Alferes <dancon.alferes@gmail.com>
2024-11-04 18:57:58 -0300

    Encaminhando parâmetros da rota para o controlador

--- Code/app_super_gestao/app/Http/Controllers/SobreNosController.php
+++ Code/app_super_gestao/app/Http/Controllers/SobreNosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SobrenosController extends Controller
+class SobreNosController extends Controller
 {
     public function sobreNos() {
         return view('site.sobre-nos');
--- Code/app_super_gestao/routes/web.php
+++ Code/app_super_gestao/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\PrincipalController;
-use App\Http\Controllers\SobrenosController;
+use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -21,16 +22,7 @@ Route::prefix('/app')->group(function() {
     Route::get('/produtos', function(){ return 'produtos'; })->name('app.produtos');
 });
 
-Route::get('/rota1', function() {
-    echo 'Rota 1';
-})->name('site.rota1');
-
-
-Route::get('/rota2', function() {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
-
-// Route::redirect('/rota2','/rota1');
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
 
 Route::fallback(function() {
     echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para a pagina inicial';
