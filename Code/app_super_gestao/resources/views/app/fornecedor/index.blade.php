d3e05a34ba3d7432ccfd3b1b56c4d49e72090232
Daniel Alferes <dancon.alferes@gmail.com>
2024-11-05 18:52:47 -0300

    Incluíndo comentários e blocos PHP puros

---
+++ Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,15 @@
+<h3>Fornecedor</h3>
+
+{{ 'Texto de teste'}}
+<?= 'Texto de teste' ?>
+
+{{-- Fica o comentario que sera descartado pelo interpretador do blade --}}
+
+@php
+    // Para comentarios de uma linha
+    /*
+        comentario para varias linhas
+    */
+
+    echo 'Texto de teste';
+@endphp
--- Code/app_super_gestao/routes/web.php
+++ Code/app_super_gestao/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\TesteController;
+use App\Http\Controllers\FornecedorController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -18,7 +19,7 @@ Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function() {
     Route::get('/clientes', function(){ return 'clientes'; })->name('app.clientes');
-    Route::get('/fornecedores', function(){ return 'fornecedores'; })->name('app.fornecedores');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
     Route::get('/produtos', function(){ return 'produtos'; })->name('app.produtos');
 });
 
