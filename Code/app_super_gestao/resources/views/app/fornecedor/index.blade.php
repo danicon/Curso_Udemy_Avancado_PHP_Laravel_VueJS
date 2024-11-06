9ad921e9cc75c0dd06d12fe22f0b3f0190705711
Daniel Alferes <dancon.alferes@gmail.com>
2024-11-06 19:43:11 -0300

    Blade - @isset

--- Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,22 +1,16 @@
 <h3>Fornecedor</h3>
 
 @php
-    // if(!<condição>) { // enquanto executa se o retorno for true
+    // if(isset($variavel) { // retornar true se a variavel estiver definida
 
     // }
 @endphp
-{{-- @dd($fornecedores) --}}
-{{-- @unless executa se o retorno for false --}}
-
-Fornecedores: {{ $fornecedores[0]['nome'] }}
-</br>
-Status: {{ $fornecedores[0]['status'] }}
-</br>
-@if( !($fornecedores[0]['status'] == 'S') )
-    Fornecedor inativo
-@endif
-<br/>
-@unless($fornecedores[0]['status'] == 'S') <!-- se oretorno da condição fpr false -->
-    Fornecedor inativo
-@endunless
-<br/>
+@isset($fornecedores)
+    Fornecedores: {{ $fornecedores[0]['nome'] }}
+    </br>
+    Status: {{ $fornecedores[0]['status'] }}
+    </br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+    @endisset
+@endisset
