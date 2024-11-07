3504b9c4033affe1ff0837c6fad209d63bd810bf
Daniel Alferes <dancon.alferes@gmail.com>
2024-11-07 18:47:23 -0300

    Operador condicional ternário

--- Code/app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ Code/app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -18,6 +18,11 @@ class FornecedorController extends Controller
                 'status' => 'S'
             ]
         ];
+
+        // condicao ? se verdadeiro : se falso;
+        // condicao ? se verdadeiro : (condicao ? se verdadeiro : se falso);
+        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
+        echo $msg;
         
         return view('app.fornecedor.index', compact('fornecedores'));
     }
