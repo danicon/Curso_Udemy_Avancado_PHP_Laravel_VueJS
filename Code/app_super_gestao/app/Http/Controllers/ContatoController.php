dcea980835a29ce49d212a56c68bb56f75f3e791
Daniel Alferes <dancon.alferes@gmail.com>
2024-12-26 18:30:14 -0300

    Entendendo o objeto Request

--- Code/app_super_gestao/app/Http/Controllers/ContatoController.php
+++ Code/app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -6,9 +6,16 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
 
-        var_dump($_POST);
+        // var_dump($_POST);
+        // dd($request);
+        echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
+        echo $request->input('nome');
+        echo '</br>';
+        echo $request->input('email');
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
