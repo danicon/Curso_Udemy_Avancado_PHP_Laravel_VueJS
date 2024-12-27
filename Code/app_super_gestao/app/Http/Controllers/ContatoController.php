febe70ecc168768a279a449906774488aadb229a
Daniel Alferes <dancon.alferes@gmail.com>
2024-12-27 18:44:27 -0300

    Gravando os dados do formulário no banco de dados

--- Code/app_super_gestao/app/Http/Controllers/ContatoController.php
+++ Code/app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
@@ -10,12 +11,39 @@ class ContatoController extends Controller
 
         // var_dump($_POST);
         // dd($request);
+        /*
         echo '<pre>';
         print_r($request->all());
         echo '</pre>';
         echo $request->input('nome');
         echo '</br>';
         echo $request->input('email');
+        */
+
+        /*
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        // print_r($contato->getAttributes());
+        $contato->save();
+        */
+
+        /*
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+        // print_r($contato->getAttributes());
+        */
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+        // $contato->save();
+        // print_r($contato->getAttributes());
+
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
