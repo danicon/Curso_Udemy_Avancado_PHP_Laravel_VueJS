c5a53c27b8af57270e7cdc5551ef19987574d33a
Daniel Alferes <dancon.alferes@gmail.com>
2025-01-31 09:04:06 -0300

    Adicionando middlewares a um grupo de rotas

--- Code/app_super_gestao/routes/web.php
+++ Code/app_super_gestao/routes/web.php
@@ -18,18 +18,10 @@ Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contat
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 
-Route::prefix('/app')->group(function() {
-    Route::middleware('autenticacao')
-        ->get('/clientes', function(){ return 'clientes'; })
-        ->name('app.clientes');
-
-    Route::middleware('autenticacao')
-        ->get('/fornecedores', [FornecedorController::class, 'index'])
-        ->name('app.fornecedores');
-
-    Route::middleware('autenticacao')
-        ->get('/produtos', function(){ return 'produtos'; })
-        ->name('app.produtos');
+Route::middleware('autenticacao')->prefix('/app')->group(function() {
+    Route::get('/clientes', function(){ return 'clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'produtos'; })->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
