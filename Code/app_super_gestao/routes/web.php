af479568ccb7533edf4f41d577d7b8776d08a968
Daniel Alferes <dancon.alferes@gmail.com>
2025-02-04 08:46:06 -0300

    Implementando o formulário de Login

--- Code/app_super_gestao/routes/web.php
+++ Code/app_super_gestao/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\TesteController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -13,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index')->middleware('log.acesso');
 Route::get('/sobre-nos', [SobrenosController::class, 'sobreNos'])->name('site.sobrenos');
+
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
-Route::get('/login', function(){ return 'Login'; })->name('site.login');
+
+Route::get('/login', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
 
 Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(function() {
