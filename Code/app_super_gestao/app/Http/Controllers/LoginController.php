346dd6bef8a0b3950b46f3098f304b85d51b853c
Daniel Alferes <dancon.alferes@gmail.com>
2025-02-05 09:53:43 -0300

    Recebendo os parâmetros de usuário e senha

--- Code/app_super_gestao/app/Http/Controllers/LoginController.php
+++ Code/app_super_gestao/app/Http/Controllers/LoginController.php
@@ -10,7 +10,22 @@ class LoginController extends Controller
         return view('site.login', ['titulo' => 'Login']);
     }
 
-    public function autenticar() {
-        return 'Chegamos até o controller de autenticação';
+    public function autenticar(Request $request) {
+        // regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        //As mensagens de feedback de validação
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+
+        // Se não passar na validação, redireciona para a página anterior
+        $request->validate($regras, $feedback);
+
+        print_r($request->all());
     }
 }
