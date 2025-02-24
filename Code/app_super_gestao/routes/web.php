b46ac1a327e77ac5211891110da70e3a175e8a73
Daniel Alferes <dancon.alferes@gmail.com>
2025-02-24 18:41:28 -0300

    Implementando o cadastro de fornecedores parte 2 (inclusão de registros)

--- Code/app_super_gestao/routes/web.php
+++ Code/app_super_gestao/routes/web.php
@@ -33,6 +33,7 @@ Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(
     Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
     Route::post('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
     Route::get('/fornecedor/adicionar', [FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
+    Route::post('/fornecedor/adicionar', [FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
 
     Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
