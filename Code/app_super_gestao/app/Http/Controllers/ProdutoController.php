2fca9906e5d6c35cc96c04fa9da078eeaf7b46b5
Daniel Alferes <dancon.alferes@gmail.com>
2025-03-31 18:45:04 -0300

    Eloquent ORM 1 para 1 - Utilizando hasOne e belongsTo com nomes não padronizados

--- Code/app_super_gestao/app/Http/Controllers/ProdutoController.php
+++ Code/app_super_gestao/app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produto;
+use App\Models\Item;
 use App\Models\ProdutoDetalhe;
 use App\Models\Unidade;
 use Illuminate\Http\Request;
@@ -14,7 +15,7 @@ class ProdutoController extends Controller
      */
     public function index(Request $request)
     {
-        $produtos = Produto::paginate(10);
+        $produtos = Item::paginate(10);
 
 
         /*
