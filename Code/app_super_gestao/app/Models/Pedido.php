e4509f89d33b3cf7b73da9c0d1b62db0b4e7065e
Daniel Alferes <dancon.alferes@gmail.com>
2025-04-12 09:48:27 -0300

    Relacionamento N para N - Colunas pivô da tabela de relacionamento (Pivot)

--- Code/app_super_gestao/app/Models/Pedido.php
+++ Code/app_super_gestao/app/Models/Pedido.php
@@ -9,7 +9,7 @@ class Pedido extends Model
 {
     public function produtos() {
         // return $this->belongsToMany('App\Models\Produto', 'pedidos_produtos');
-        return $this->belongsToMany('App\Models\Item', 'pedidos_produtos', 'pedido_id', 'produto_id');
+        return $this->belongsToMany('App\Models\Item', 'pedidos_produtos', 'pedido_id', 'produto_id')->withPivot('created_at', 'updated_at');
         /*
             1 - Modelo do relacionemnto NxN em relação o Modelo que estamos implementando
             2 - É a tabela auxiliar que armazena os registros de relacionamento
--- Code/app_super_gestao/resources/views/app/pedido_produto/create.blade.php
+++ Code/app_super_gestao/resources/views/app/pedido_produto/create.blade.php
@@ -29,6 +29,7 @@
                         <tr>
                             <th>ID</th>
                             <th>Nome do Produto</th>
+                            <th>Data de inclusão do item no pedido</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -36,6 +37,7 @@
                             <tr>
                                 <td>{{ $produto->id }}</td>
                                 <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->pivot->created_at->format('d/m/Y') }}</td>
                             </tr>
                         @endforeach
                     </tbody>
