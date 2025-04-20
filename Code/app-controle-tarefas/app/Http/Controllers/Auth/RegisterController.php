88219e0415568eee1621f8c15594975f387238b0
Daniel Alferes <dancon.alferes@gmail.com>
2025-04-20 09:33:18 -0300

    Registrando novos usuários e efetuando o login na aplicação

--- Code/app-controle-tarefas/app/Http/Controllers/Auth/RegisterController.php
+++ Code/app-controle-tarefas/app/Http/Controllers/Auth/RegisterController.php
@@ -21,7 +21,7 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+    use RegistersUsers; //trait (include)
 
     /**
      * Where to redirect users after registration.
