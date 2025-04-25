ac752d8dd92d49be49e53b295233fb00f372548e
Daniel Alferes <dancon.alferes@gmail.com>
2025-04-25 11:18:15 -0300

    Ajuste url

--- Code/app-controle-tarefas/resources/views/vendor/mail/html/header.blade.php
+++ Code/app-controle-tarefas/resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="{{ $url }}:8000/img/logo.png" style="display: inline-block;">
 @if (trim($slot) === 'Controle de Tarefas')
 <img src="http://localhost:8000/img/logo.png" class="logo" alt="Laravel Logo">
 @else
