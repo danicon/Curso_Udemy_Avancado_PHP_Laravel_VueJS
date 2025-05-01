dc409b682080482f5a1ab2dd77b5bea0ad57caed
Daniel Alferes <dancon.alferes@gmail.com>
2025-05-01 11:15:51 -0300

    Modificando a rota home da aplicação (redirectTo)

--- Code/app-controle-tarefas/routes/web.php
+++ Code/app-controle-tarefas/routes/web.php
@@ -11,9 +11,9 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
-    ->name('home')
-    ->middleware('verified');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+//     ->name('home')
+//     ->middleware('verified');
 
 // Route::resource('tarefa', 'App\Http\Controllers\TarefaController')->middleware('auth');
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController')
