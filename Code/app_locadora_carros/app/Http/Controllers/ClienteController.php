f190ae47a95d79ef103943d0f0a2c7b33b760041
Daniel Alferes <dancon.alferes@gmail.com>
2025-06-08 09:06:16 -0300

    Rotas e a diferença entre Route::resource e Route::apiResource

--- Code/app_locadora_carros/app/Http/Controllers/ClienteController.php
+++ Code/app_locadora_carros/app/Http/Controllers/ClienteController.php
@@ -17,14 +17,6 @@ class ClienteController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreClienteRequest $request)
@@ -36,14 +28,6 @@ class ClienteController extends Controller
      * Display the specified resource.
      */
     public function show(Cliente $cliente)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Cliente $cliente)
     {
         //
     }
--- Code/app_locadora_carros/routes/api.php
+++ Code/app_locadora_carros/routes/api.php
@@ -2,11 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\LocacaoController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ModeloController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/', function () {
-    return ['Chegamos até aqui' => 'SIM'];
-});
+// Route::resource('cliente', ClienteController::class);
+Route::apiResource('cliente', ClienteController::class);
+Route::apiResource('carro', CarroController::class);
+Route::apiResource('locacao', LocacaoController::class);
+Route::apiResource('marca', MarcaController::class);
+Route::apiResource('modelo', ModeloController::class);
