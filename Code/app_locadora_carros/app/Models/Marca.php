98d48bc85e46597168f9d4a512f113ab510d8cfa
Daniel Alferes <dancon.alferes@gmail.com>
2025-06-14 09:03:45 -0300

    Implementando as regras de validação (Rules) no Model

--- Code/app_locadora_carros/app/Models/Marca.php
+++ Code/app_locadora_carros/app/Models/Marca.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     protected $fillable = ['nome', 'imagem'];
+
+    public function rules() {
+        return [
+            'nome' => 'required|unique:marcas|min:3',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedback() {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca já existe',
+            'nome.min' => 'O nome da marca deve ter no mínimo 3 caracteres',
+        ];
+    }
 }
