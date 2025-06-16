326f48838f390e35fb5d3744e8e8d1db59ca5b8e
Daniel Alferes <dancon.alferes@gmail.com>
2025-06-16 18:34:33 -0300

    Upload de arquivos - Implementando o upload de imagens parte 1

--- Code/app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ Code/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -43,7 +43,14 @@ class MarcaController extends Controller
         $request->validate($this->marca->rules(), $this->marca->feedback());
         //stateless
 
-        $marca = $this->marca->create($request->all());
+        // dd($request->nome);
+        // dd($request->get('nome'));
+        // dd($request->input('nome'));
+
+        // dd($request->imagem);
+        dd($request->file('imagem'));
+
+        // $marca = $this->marca->create($request->all());
         return response()->json($marca, 201);
     }
 
