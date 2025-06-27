a66fe8736b3336e7b3fba0b28f938536702ade0f
Daniel Alferes <dancon.alferes@gmail.com>
2025-06-27 18:42:32 -0300

    Refactoring do endpoint update de marca e modelo

--- Code/app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ Code/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -144,10 +144,19 @@ class MarcaController extends Controller
         $imagem_urn = $imagem->store('imagens', 'public');
         // dd($imagem_urn);
 
+        //preencher o objeto $marca com os dados do request
+        // dd($request->all());
+        $marca->fill($request->all());
+        $marca->imagem = $imagem_urn;
+        // dd($marca->getAttributes());
+        $marca->save();
+
+        /*
         $marca->update([
             'nome' => $request->nome,
             'imagem' => $imagem_urn
         ]);
+        */
 
         return response()->json($marca, 200);
     }
