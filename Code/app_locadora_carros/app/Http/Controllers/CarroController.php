c1dfde7909929e5cf3165f2fb2b76c939379ca90
Daniel Alferes <dancon.alferes@gmail.com>
2025-07-04 19:04:42 -0300

    API WebService Rest para o Resource Carro

--- Code/app_locadora_carros/app/Http/Controllers/CarroController.php
+++ Code/app_locadora_carros/app/Http/Controllers/CarroController.php
@@ -3,17 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carro;
-use App\Http\Requests\StoreCarroRequest;
-use App\Http\Requests\UpdateCarroRequest;
+use App\Repositories\CarroRepository;
+use Illuminate\Http\Request;
 
 class CarroController extends Controller
 {
+    private $carro;
+    public function __construct(Carro $carro) {
+        $this->carro = $carro;
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $carroRepository = new CarroRepository($this->carro);
+
+        if($request->has('atributos_modelo')) {
+            $atributos_modelo = 'modelo:id,'.$request->atributos_modelo;
+            $carroRepository->selectAtributosRegistroRelacionados($atributos_modelo);
+        } else {
+            $carroRepository->selectAtributosRegistroRelacionados('modelo');
+        }
+
+        if($request->has('filtro')) {
+            $carroRepository->filtro($request->filtro);
+        }
+
+        if($request->has('atributos')) {
+            $carroRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($carroRepository->getResultado(), 200);
     }
 
     /**
@@ -27,17 +49,31 @@ class CarroController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCarroRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->carro->rules());
+
+        $carro = $this->carro->create([
+            'modelo_id' => $request->modelo_id,
+            'placa' => $request->placa,
+            'disponivel' => $request->disponivel,
+            'km' => $request->km
+        ]);
+
+        return response()->json($carro, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Carro $carro)
+    public function show($id)
     {
-        //
+        $carro = $this->carro->with('modelo')->find($id);
+        if($carro === null) {
+            return response()->json(['erro' => "Recurso pesquisado não existe"], 404);
+        }
+
+        return response()->json($carro, 200);
     }
 
     /**
@@ -51,16 +87,49 @@ class CarroController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCarroRequest $request, Carro $carro)
+    public function update(Request $request, $id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if($carro === null) {
+            return response()->json(['erro' => 'Impossievel realizar a atualização. Recurso solicitado não existe.'], 404);
+        }
+
+        if($request->method() === 'PATCH') {
+
+            $regrasDinamicas = array();
+
+            // percorrendo todas as regras no Model
+            foreach($carro->rules() as $input => $regras) {
+                //coletar apenas a regras aplicaveis aos parametros parcias da requisição PATCH
+                if(array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regras;       
+                }
+            }
+
+            $request->validate($regrasDinamicas);
+        } else {
+            $request->validate($carro->rules());
+        }
+        
+        $carro->fill($request->all());
+        $carro->save();
+
+        return response()->json($carro, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Carro $carro)
+    public function destroy($id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if($carro === null) {
+            return response()->json(['erro' => 'Impossievel realizar a exclusão. O Recurso solicitado não existe.'], 404);
+        }
+
+        $carro->delete();
+        return response()->json(['msg' => 'O carro foi removida com sucesso!'], 200);
     }
 }
