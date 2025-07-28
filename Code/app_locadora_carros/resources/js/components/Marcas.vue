02ad694725e4dff6662f1005e903243224e83c05
Daniel Alferes <dancon.alferes@gmail.com>
2025-07-28 08:54:08 -0300

    Criando o card de listagem de registros

--- Code/app_locadora_carros/resources/js/components/Marcas.vue
+++ Code/app_locadora_carros/resources/js/components/Marcas.vue
@@ -2,7 +2,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+
+                <!-- Inicio do card de busca -->
+                <div class="card mb-3">
                     <div class="card-header">Busca de marcas</div>
 
                     <div class="card-body">
@@ -31,6 +33,54 @@
                     </div>  
                     
                 </div>
+                <!-- Final do card de busca -->
+
+                <!-- Inicio do card de listagem de marcas -->
+                <div class="card">
+                    <div class="card-header">Relação de marcas</div>
+
+                    <div class="card-body">
+
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">3</th>
+                                <td>John</td>
+                                <td>Doe</td>
+                                <td>@social</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                                 
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary btn-sm float-end">Adicionar</button>
+                    </div>  
+                    
+                </div>
+                <!-- Fim do card de listagem de marcas -->
+
             </div>
         </div>
     </div>
