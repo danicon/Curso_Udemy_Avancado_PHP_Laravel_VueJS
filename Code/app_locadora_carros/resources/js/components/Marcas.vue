dc489f5d290cbc361cd8faa5aee14706e0461e9b
Daniel Alferes <dancon.alferes@gmail.com>
2025-08-02 09:11:41 -0300

    Adicionando os inputs no modal de registros de marcas

--- Code/app_locadora_carros/resources/js/components/Marcas.vue
+++ Code/app_locadora_carros/resources/js/components/Marcas.vue
@@ -45,7 +45,28 @@
             </div>
         </div>
 
-        <modal-component id="modalMarca" titulo="Adicionar marca"></modal-component>
+        <modal-component id="modalMarca" titulo="Adicionar marca">
+
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp" texto-ajuda="Informe o nome da marca">
+                        <input type="text" class="form-control" id="inputNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca">
+                    </input-container-component> 
+                </div>
+
+                <div class="form-group">
+                    <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp" texto-ajuda="Selecione uma imagem no formato PNG">
+                        <input type="file" class="form-control" id="inputNome" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem">
+                    </input-container-component> 
+                </div>
+            </template>
+
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Salvar</button>
+            </template>
+
+        </modal-component>
 
     </div>
 </template>
