6663f7d820a70a8c9f671b9720a450e7f44c495c
Daniel Alferes <dancon.alferes@gmail.com>
2025-08-16 14:19:09 -0300

    Extra - Adicionando metadados aos títulos da listagem

--- Code/app_locadora_carros/resources/js/components/Table.vue
+++ Code/app_locadora_carros/resources/js/components/Table.vue
@@ -5,10 +5,22 @@
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col" v-for="t, key in titulos" :key="key" class="text-uppercase">{{t}}</th>
+                    <th scope="col" v-for="t, key in titulos" :key="key">{{t.titulo}}</th>
                 </tr>
             </thead>
             <tbody>
+
+                <tr v-for="obj, chave in dadosFiltrados" :key="chave">
+                    <td v-for="valor, chaveValor in obj" :key="chaveValor">
+                        <span v-if="titulos[chaveValor].tipo == 'texto'">{{valor}}</span>
+                        <span v-if="titulos[chaveValor].tipo == 'data'">{{'...'+valor}}</span>
+                        <span v-if="titulos[chaveValor].tipo == 'imagem'">
+                            <img :src="'/storage/'+valor" width="30" height="30">
+                        </span>
+                    </td>
+                </tr>
+
+                <!--
                 <tr v-for="obj in dados" :key="obj.id">
                     <td  v-for="valor, chave in filtrarObjeto(obj)" :key="chave">
                        <span v-if="chave == 'imagem'">
@@ -19,12 +31,12 @@
                        </span>
                     </td>
 
-                    <!--
                     <th scope="row">{{m.id}}</th>
                     <td>{{m.nome}}</td>
                     <td><img :src="'/storage/' + m.imagem" width="30" height="30"></td>
-                    -->
+                    
                 </tr>
+                -->
             </tbody>
         </table>
 
@@ -35,15 +47,43 @@
 <script>
     export default {
         props: ['dados', 'titulos'],
-        methods: {
-            filtrarObjeto(obj) {
-                if (!obj) return {};
-                return Object.fromEntries(
-                    Object.entries(obj).filter(([chave]) =>
-                    this.titulos.includes(chave)
-                    )
-                );
+        computed: {
+            dadosFiltrados() {
+
+                let campos = Object.keys(this.titulos)
+                let dadosFiltrados = []
+                // console.log(campos)
+                // console.log(this.dados)
+
+                this.dados.map((item, chave) => {
+                    // console.log(chave, item)
+
+                    let itemFiltrado = {}
+                    campos.forEach(campo => {
+                        // console.log(campo)
+                        itemFiltrado[campo] = item[campo] //utilizar a sintaxe de array para atribuir valores a objetos
+                        // console.log(chave, item, campo)
+                    })
+                    
+                    dadosFiltrados.push(itemFiltrado)
+                    
+                })
+
+                // console.log(dadosFiltrados)
+
+                return dadosFiltrados //retorne um array de objetos
             }
         }
+
+        // methods: {
+        //     filtrarObjeto(obj) {
+        //         if (!obj) return {};
+        //         return Object.fromEntries(
+        //             Object.entries(obj).filter(([chave]) =>
+        //             this.titulos.includes(chave)
+        //             )
+        //         );
+        //     }
+        // }
     }
 </script>
